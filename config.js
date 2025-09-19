64b1221e6c5f72d575f2e8f84e73b801694711c7
Nikhil Kumar <nik326990@gmail.com>
2025-09-19 12:54:56 +0530

    de

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 // const API_URL = 'https://fvvcbrpm-4000.inc1.devtunnels.ms/'
 // const API_URL = 'https://chauhansonsjewellers.com/'
-const API_URL = 'http://31.97.237.55:4000'
+// const API_URL = 'http://31.97.237.55:4000'
+const API_URL = 'https://ektriptobantihai.in'
+
 
 
 export default API_URL;
