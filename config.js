8ad40eb1eda0a75e498b60ff0fb47d209c85212d
shiv_kumar <shiva90154@gmail.com>
2025-12-11 17:15:11 +0530

    chnages 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// const API_URL = 'http://localhost:4000'
-const API_URL = 'https://backend.chauhansonsjewellers.com'
+const API_URL = 'http://localhost:4000'
+// const API_URL = 'https://backend.chauhansonsjewellers.com'
 
 
 export default API_URL;
